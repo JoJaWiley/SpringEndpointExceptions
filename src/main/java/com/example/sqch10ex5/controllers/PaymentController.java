47d22beb4aad1199a257730196df01cafdd0ca31
JoJaWiley <joshua.wiley@summitoic.edu>
2023-09-10 18:48:22 -0500

    PaymentController now gets data from client using request body

--- src/main/java/com/example/sqch10ex5/controllers/PaymentController.java
+++ src/main/java/com/example/sqch10ex5/controllers/PaymentController.java
@@ -8,24 +8,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
 
 @RestController
 public class PaymentController {
 
-    private final PaymentService paymentService;
+    private static Logger logger = Logger.getLogger(PaymentController.class.getName());
+    @PostMapping("/payment")
+    public ResponseEntity<PaymentDetails> makePayment(@RequestBody PaymentDetails paymentDetails) {
 
-    public PaymentController(PaymentService paymentService) {
-        this.paymentService = paymentService;
-    }
-
-    @GetMapping("/payment")
-    public ResponseEntity<PaymentDetails> makePayment() {
-            PaymentDetails paymentDetails = paymentService.processPayment();
+        logger.info("Received payment " + paymentDetails.getAmount());
 
             return ResponseEntity
                     .status(HttpStatus.ACCEPTED)
                     .body(paymentDetails);
-
     }
 }
